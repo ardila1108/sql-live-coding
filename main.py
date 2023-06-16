57f5eeb4c591bb7f74eaf16bd7da55626487ad16
Carlos Ardila <ardila11.08@gmail.com>
2023-06-16 11:08:19 -0500

    Add questions of different difficulties to tourism data

--- main.py
+++ main.py
@@ -4,23 +4,19 @@ from streamlit_ace import st_ace
 from pandasql import sqldf
 from pandasql.sqldf import PandaSQLException
 
-from components import EntityRelationshipDiagram
+from components.challenge import ChallengePanel
 
 params = st.experimental_get_query_params()
 
 st.title("SQL Live coding tool")
 
 if "db" in params:
-    st.write("""
-    All your assessments will be done based on the Entity Relationship Diagram presented below.
-    Take your time to understand it and ask any questions you see fit.
-    """)
 
-    erd = EntityRelationshipDiagram(params.get("db")[0])
-    erd.render()
+    challenge_panel = ChallengePanel(params.get("db")[0], params.get("difficulty", [None])[0])
+    challenge_panel.render()
 
     st.write("""
-    In the code box below you can execute your SQL code and see your results below.
+    In the following code box you can execute your SQL code and see your results below.
     Complete the tasks given by your interviewer.
     """)
 
@@ -28,7 +24,7 @@ if "db" in params:
 
     if res:
         try:
-            df = sqldf(res, erd.file_dict)
+            df = sqldf(res, challenge_panel.erd.file_dict)
             st.subheader("Result")
             st.write(df)
         except PandaSQLException as e:
